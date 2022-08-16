546fe32a8573cbc058dfd220d19505846fb04ce9
aphness007 <aphness007@gmail.com>
2022-08-17 00:01:13 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.lowerNum;
+        this.greaterNum;
+        this.half_way;
+    }
 
     setRange(min, max) {
-
+        this.lowerNum= min;
+        this.greaterNum = max;
+       
     }
 
     guess() {
-
+        return this.half_way = Math.round((this.greaterNum + this.lowerNum) / 2);
+        // return Math.ceil(this.half_way );
     }
+    
 
     lower() {
-
+    this.greaterNum = this.half_way;
     }
 
     greater() {
-
+    this.lowerNum = this.half_way;
     }
 }
 
